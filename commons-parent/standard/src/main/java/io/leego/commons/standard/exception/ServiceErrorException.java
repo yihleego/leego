664d4397588e9738e393062e81526f15debcab64
leego <leegodev@hotmail.com>
2022-08-18 15:27:37 +0800

    Split ServiceException

--- commons-parent/standard/src/main/java/io/leego/commons/standard/exception/ServiceErrorException.java
+++ commons-parent/standard/src/main/java/io/leego/commons/standard/exception/ServiceErrorException.java
@@ -7,100 +7,100 @@ import java.io.Serial;
 /**
  * @author Leego Yih
  */
-public class ServiceException extends RuntimeException {
+public class ServiceErrorException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
     protected final Integer code;
 
     /**
-     * Constructs a new service exception with no detail message.
+     * Constructs a new exception with no detail message.
      */
-    public ServiceException() {
+    public ServiceErrorException() {
         super("Oops! An error has occurred.");
         this.code = 0;
     }
 
     /**
-     * Constructs a new service exception with the specified detail message.
+     * Constructs a new exception with the specified detail message.
      *
      * @param message the detail message.
      */
-    public ServiceException(String message) {
+    public ServiceErrorException(String message) {
         super(message);
         this.code = 0;
     }
 
     /**
-     * Constructs a new service exception with the specified detail message and cause.
+     * Constructs a new exception with the specified detail message and cause.
      *
      * @param message the detail message.
      * @param cause   the cause.
      */
-    public ServiceException(String message, Throwable cause) {
+    public ServiceErrorException(String message, Throwable cause) {
         super(message, cause);
         this.code = 0;
     }
 
     /**
-     * Constructs a new service exception with the cause.
+     * Constructs a new exception with the cause.
      *
      * @param cause the cause.
      */
-    public ServiceException(Throwable cause) {
+    public ServiceErrorException(Throwable cause) {
         super(cause);
         this.code = 0;
     }
 
     /**
-     * Constructs a new service exception with the error code and specified detail message.
+     * Constructs a new exception with the error code and specified detail message.
      *
      * @param code    the error code.
      * @param message the detail message.
      */
-    public ServiceException(Integer code, String message) {
+    public ServiceErrorException(Integer code, String message) {
         super(message);
         this.code = code;
     }
 
     /**
-     * Constructs a new service exception with the error code and cause.
+     * Constructs a new exception with the error code and cause.
      *
      * @param code  the error code.
      * @param cause the cause.
      */
-    public ServiceException(Integer code, Throwable cause) {
+    public ServiceErrorException(Integer code, Throwable cause) {
         super(cause);
         this.code = code;
     }
 
     /**
-     * Constructs a new service exception with the error code, specified detail message and cause.
+     * Constructs a new exception with the error code, specified detail message and cause.
      *
      * @param code    the error code.
      * @param message the detail message.
      * @param cause   the cause.
      */
-    public ServiceException(Integer code, String message, Throwable cause) {
+    public ServiceErrorException(Integer code, String message, Throwable cause) {
         super(message, cause);
         this.code = code;
     }
 
     /**
-     * Constructs a new service exception with the error.
+     * Constructs a new exception with the error.
      *
      * @param error the error.
      */
-    public ServiceException(Error error) {
+    public ServiceErrorException(Error error) {
         this(error.getCode(), error.getMessage());
     }
 
     /**
-     * Constructs a new service exception with the error and cause.
+     * Constructs a new exception with the error and cause.
      *
      * @param error the error.
      * @param cause the cause.
      */
-    public ServiceException(Error error, Throwable cause) {
+    public ServiceErrorException(Error error, Throwable cause) {
         this(error.getCode(), error.getMessage(), cause);
     }
 
