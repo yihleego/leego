4dd4fbd63b3e1f61dc58812440d63aeaa4f92612
leego <leegodev@hotmail.com>
2022-08-28 00:46:04 +0800

    Add `spring-cloud-parent`

--- commons-parent/json/src/main/java/io/leego/commons/json/JsonUtils.java
+++ commons-parent/json/src/main/java/io/leego/commons/json/JsonUtils.java
@@ -13,6 +13,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.type.SimpleType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +42,7 @@ public final class JsonUtils {
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .addModules(new Jdk8Module(), new JavaTimeModule(), new ParameterNamesModule())
                 .build();
         prettyWriter = mapper.writerWithDefaultPrettyPrinter();
     }
