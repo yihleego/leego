09d40c19219eec75d52382cc066d377a5f1860f0
leego <leegodev@hotmail.com>
2022-08-28 16:34:48 +0800

    Polish `NamedThreadFactory`

--- commons-parent/standard/src/main/java/io/leego/commons/standard/concurrent/NamedThreadFactory.java
+++ commons-parent/standard/src/main/java/io/leego/commons/standard/concurrent/NamedThreadFactory.java
@@ -1,5 +1,6 @@
 package io.leego.commons.standard.concurrent;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,51 +8,48 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author Leego Yih
  */
 public class NamedThreadFactory implements ThreadFactory {
-    private static final String DEFAULT_GROUP_NAME = "default";
     private final AtomicLong threadNumber = new AtomicLong(0);
     private final ThreadGroup group;
     private final String namePrefix;
     private final boolean daemon;
 
+    public NamedThreadFactory(String namePrefix) {
+        this(namePrefix, false);
+    }
+
+    public NamedThreadFactory(String groupName, String namePrefix) {
+        this(groupName, namePrefix, false);
+    }
+
     public NamedThreadFactory(String namePrefix, boolean daemon) {
-        this.group = new ThreadGroup(DEFAULT_GROUP_NAME);
-        this.namePrefix = namePrefix;
+        this.group = null;
+        this.namePrefix = Objects.requireNonNull(namePrefix);
         this.daemon = daemon;
     }
 
     public NamedThreadFactory(String groupName, String namePrefix, boolean daemon) {
-        this.group = new ThreadGroup(groupName);
-        this.namePrefix = namePrefix;
+        this.group = new ThreadGroup(Objects.requireNonNull(groupName));
+        this.namePrefix = Objects.requireNonNull(namePrefix);
         this.daemon = daemon;
     }
 
-    public static ThreadFactory build(String groupName, String namePrefix, boolean daemon) {
-        return new NamedThreadFactory(groupName, namePrefix, daemon);
-    }
-
-    public static ThreadFactory build(String namePrefix, boolean daemon) {
-        return new NamedThreadFactory(namePrefix, daemon);
-    }
-
-    public static ThreadFactory build(boolean daemon) {
-        return new NamedThreadFactory(null, daemon);
-    }
-
-    public static ThreadFactory build() {
-        return new NamedThreadFactory(null, false);
+    public NamedThreadFactory(ThreadGroup group, String namePrefix, boolean daemon) {
+        this.group = group;
+        this.namePrefix = Objects.requireNonNull(namePrefix);
+        this.daemon = daemon;
     }
 
     private String nextName() {
-        if (namePrefix == null || namePrefix.length() == 0) {
-            return group.getName() + "-" + threadNumber.getAndIncrement();
-        } else {
+        if (group != null) {
             return group.getName() + "-" + namePrefix + "-" + threadNumber.getAndIncrement();
+        } else {
+            return namePrefix + "-" + threadNumber.getAndIncrement();
         }
     }
 
     @Override
-    public Thread newThread(Runnable runnable) {
-        Thread thread = new Thread(group, runnable, nextName());
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(group, r, nextName());
         thread.setDaemon(daemon);
         if (thread.getPriority() != Thread.NORM_PRIORITY) {
             thread.setPriority(Thread.NORM_PRIORITY);
