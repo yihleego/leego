d7ea0fb979edf52d80a56e3f3a4998a79161aa31
leego <leegodev@hotmail.com>
2022-09-21 10:18:23 +0800

    Add base entities

---
+++ support-parent/jpa-support/src/main/java/io/leego/support/jpa/entity/BaseLongIdEntity.java
@@ -0,0 +1,22 @@
+package io.leego.support.jpa.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Leego Yih
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
+public abstract class BaseLongIdEntity extends BaseEntity<Long> {
+    @Override
+    public Long getId() {
+        return id;
+    }
+}
