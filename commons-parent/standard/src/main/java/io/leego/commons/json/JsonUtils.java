d6739c93c569385cce76e5075fda3f48ccef8f74
leego <leegodev@hotmail.com>
2022-09-22 22:02:37 +0800

    Remove JavaTimeModule

--- commons-parent/standard/src/main/java/io/leego/commons/json/JsonUtils.java
+++ commons-parent/standard/src/main/java/io/leego/commons/json/JsonUtils.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.type.SimpleType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import java.io.IOException;
@@ -42,7 +41,7 @@ public final class JsonUtils {
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
-                .addModules(new Jdk8Module(), new JavaTimeModule(), new ParameterNamesModule())
+                .addModules(new Jdk8Module(), new ParameterNamesModule())
                 .build();
         pretty = mapper.writerWithDefaultPrettyPrinter();
     }
