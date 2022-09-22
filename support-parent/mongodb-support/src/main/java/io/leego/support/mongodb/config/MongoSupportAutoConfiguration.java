44cbff4b3139cc1a559955a3ebc3e8f2dbe3e803
leego <leegodev@hotmail.com>
2022-09-22 21:35:12 +0800

    Lazy garbage

--- support-parent/mongodb-support/src/main/java/io/leego/support/mongodb/config/MongoSupportAutoConfiguration.java
+++ support-parent/mongodb-support/src/main/java/io/leego/support/mongodb/config/MongoSupportAutoConfiguration.java
@@ -1,6 +1,5 @@
 package io.leego.support.mongodb.config;
 
-import io.leego.support.mongodb.entity.Garbage;
 import io.leego.support.mongodb.listener.MongoGarbageCollectionEventListener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +9,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.IndexResolver;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
 
 /**
  * @author Leego Yih
@@ -23,14 +19,8 @@ public class MongoSupportAutoConfiguration {
     @EventListener(ContextRefreshedEvent.class)
     public void contextRefreshedEvent(ContextRefreshedEvent event) {
         ApplicationContext applicationContext = event.getApplicationContext();
-        // Remove default type mapper
         MappingMongoConverter mappingMongoConverter = applicationContext.getBean(MappingMongoConverter.class);
         mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
-        // Ensure indexes
-        MongoTemplate mongoTemplate = applicationContext.getBean(MongoTemplate.class);
-        IndexResolver indexResolver = new MongoPersistentEntityIndexResolver(mongoTemplate.getConverter().getMappingContext());
-        IndexOperations indexOps = mongoTemplate.indexOps(Garbage.class);
-        indexResolver.resolveIndexFor(Garbage.class).forEach(indexOps::ensureIndex);
     }
 
     @Bean
